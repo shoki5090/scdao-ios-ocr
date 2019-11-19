84111c42f025747795abc23f1d96f2079b17c681
Masa Iwasa <miwasa20@bu.edu>
2019-11-19 18:54:48 -0500

    save added

--- swift_test/test/ViewController.swift
+++ swift_test/test/ViewController.swift
@@ -7,58 +7,182 @@
 
 import UIKit
 
-class ViewController: UIViewController,
-    UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    // ctr drag from button, we can ad log in function as the action of the button
-//    @IBOutlet weak var login: UIButton!
+//class ViewController: UIViewController,
+//    UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+//    // ctr drag from button, we can ad log in function as the action of the button
+////    @IBOutlet weak var login: UIButton!
+////    @IBOutlet weak var imagePicked: UIImageView!
 //    @IBOutlet weak var imagePicked: UIImageView!
-//    @IBOutlet weak var imagePicked: UIImageView!
+//
+//    @IBAction func openCameraButton(_ sender: Any) {
+//        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            let imagePicker = UIImagePickerController()
+//                   imagePicker.delegate = self
+//                   imagePicker.sourceType = .camera;
+//                   imagePicker.allowsEditing = false
+//            self.present(imagePicker, animated: true, completion: nil)
+//               }
+//    }
+//
+//
+//    @IBAction func openPhotoLibraryButton(_ sender: Any) {
+//        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+//            let imagePicker = UIImagePickerController()
+//            imagePicker.delegate = self
+//            imagePicker.sourceType = .photoLibrary;
+//            imagePicker.allowsEditing = true
+//            self.present(imagePicker, animated: true, completion: nil)
+//        }
+//    }
+//
+//    internal func imagePickerController(_ picker: UIImagePickerController,
+//       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+//           imagePicked.image = image
+//
+//        dismiss(animated:true, completion: nil)
+//        guard (info[.originalImage] as? UIImage) != nil else {
+//            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+//        }
+//    }
+////     Save and getdocument using this article : https://www.hackingwithswift.com/example-code/media/how-to-save-a-uiimage-to-a-file-using-jpegdata-and-pngdata
+//
+////    func getDocumentsDirectory() -> URL {
+////        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+////        return paths[0]
+////    }
+//
+//    @IBAction func saveButt(sender: AnyObject) {
+//
+//        if let image = UIImage(image) {
+//            if let data = image.pngData() {
+//                let filename = getDocumentsDirectory().appendingPathComponent("copy.png")
+//                try? data.write(to: filename)
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//}
+//
 
-    @IBAction func openCameraButton(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let imagePicker = UIImagePickerController()
-                   imagePicker.delegate = self
-                   imagePicker.sourceType = .camera;
-                   imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true, completion: nil)
-               }
-    }
-    
-    
-    @IBAction func openPhotoLibraryButton(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary;
-            imagePicker.allowsEditing = true
-            self.present(imagePicker, animated: true, completion: nil)
-        }
-    }
-    
-    internal func imagePickerController(_ picker: UIImagePickerController,
-       didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        guard (info[.originalImage] as? UIImage) != nil else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-    }
-//     Save and getdocument using this article : https://www.hackingwithswift.com/example-code/media/how-to-save-a-uiimage-to-a-file-using-jpegdata-and-pngdata
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-    
-    @IBAction func saveButt(sender: AnyObject) {
-        if let image = UIImage(named: "example.png") {
-            if let data = image.pngData() {
-                let filename = getDocumentsDirectory().appendingPathComponent("copy.png")
-                try? data.write(to: filename)
-            }
-        }
-    }
-    
-    
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    
+    @IBOutlet var cameraView : UIImageView!
+    
+    @IBOutlet weak var mylabel: UILabel?
+        
+    //    @IBOutlet weakvar mylabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mylabel?.text = "Tap the Scan Document to scan scan a ducment"
+        
+    }
+    
+    // カメラの撮影開始
+    @IBAction func startCamera(_ sender : AnyObject) {
+        
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.camera
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.camera){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+        }
+        else{
+            mylabel?.text = "error"
+            
+        }
+    }
+    
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        if let pickedImage = info[.originalImage]
+            as? UIImage {
+            
+            cameraView.contentMode = .scaleAspectFit
+            cameraView.image = pickedImage
+            
+        }
+
+        //閉じる処理
+        imagePicker.dismiss(animated: true, completion: nil)
+        mylabel?.text = "Tap the [Save] to save a picture"
+        
+    }
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        mylabel?.text = "Canceled"
+    }
+    
+    
+    // 写真を保存
+    @IBAction func savePicture(_ sender : AnyObject) {
+        let image:UIImage! = cameraView.image
+        
+        if image != nil {
+            UIImageWriteToSavedPhotosAlbum(
+                image,
+                self,
+                #selector(ViewController.image(_:didFinishSavingWithError:contextInfo:)),
+                nil)
+        }
+        else{
+            mylabel?.text = "image Failed !"
+        }
+        
+    }
+    
+    // 書き込み完了結果の受け取り
+    @objc func image(_ image: UIImage,
+                     didFinishSavingWithError error: NSError!,
+                     contextInfo: UnsafeMutableRawPointer) {
+        
+        if error != nil {
+            print(error.code)
+            mylabel?.text = "Save Failed !"
+        }
+        else{
+            mylabel?.text = "Save Succeeded"
+        }
+    }
+    
+    // アルバムを表示
+    @IBAction func showAlbum(_ sender : AnyObject) {
+        let sourceType:UIImagePickerController.SourceType =
+            UIImagePickerController.SourceType.photoLibrary
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerController.SourceType.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            self.present(cameraPicker, animated: true, completion: nil)
+            
+            mylabel?.text = "Tap the [Start] to save a picture"
+        }
+        else{
+            mylabel?.text = "error"
+            
+        }
+        
+    }
 
 }
-
